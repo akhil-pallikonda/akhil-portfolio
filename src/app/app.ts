da9d294986fa7726ef9f10191381aaf6aa2f4e1f
Akhil Pallikonda <pallikondaakhil9@gmail.com>
2026-06-03 00:36:49 +0530

    Experience Journey

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { Introduction } from './introduction/introduction'; // Updated import
 import { About } from './about/about';
 import { Skills } from './skills/skills';
+import { Experience } from './experience/experience';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Introduction, About, Skills], // Updated imports array
+  imports: [Introduction, About, Skills, Experience], // Updated imports array
   template: `
     <main class="min-h-screen bg-appleBlack">
       <app-introduction />  <app-about />
       <app-skills />
+      <app-experience />
     </main>
   `
 })
-export class App {}
+export class App { }
